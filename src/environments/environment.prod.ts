cce6b3316165bed22d0585361d4a81b518bf4376
Martin Sosa <martin_sc2@msn.com>
2022-02-17 20:59:34 -0700

    lighthouse

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  endpoint: 'http://127.0.0.1:8000/',
+  client_id: 2,
+  client_secret: 'H6EE17WlixZKlz8bSaqHF2etKgVgWkvmNjVHcPpi',
+  brand_color: '#ff7f00'
 };
